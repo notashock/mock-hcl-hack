9ef38cd3fd07628e92606c3731583f73aebf2cbe
notashock <ashubavireddy2015@gmail.com>
2026-05-12 11:49:01 +0530

    initial commit

---
+++ server/src/test/java/com/library/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
